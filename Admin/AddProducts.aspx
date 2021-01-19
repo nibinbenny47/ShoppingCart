2a39aba358e6725dd63e22d655de2eef8ef6f1c3
Nibin Benny <nibinbenny47@gmail.com>
2021-01-19 21:34:18 +0530

    products added to the table

---
+++ Admin/AddProducts.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/MasterPage.master" AutoEventWireup="true" CodeFile="AddProducts.aspx.cs" Inherits="Admin_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <table>
+        <tr>
+            <td>Name</td>
+            <td>
+                <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+
+                
+            </td>
+            
+        </tr>
+         
+        <tr>
+            <td>Photo</td>
+            <td>
+                <asp:FileUpload ID="fileImage" runat="server" />
+
+            </td>
+        </tr>
+
+         
+        <tr>
+            <td>Item</td>
+            <td>
+                <asp:DropDownList ID="ddlItem" runat="server"></asp:DropDownList>
+
+            </td>
+        </tr>
+        <tr>
+            <td>Desription</td>
+            <td>
+                <asp:TextBox ID="txtDescription" runat="server"></asp:TextBox>
+
+            </td>
+        </tr>
+         <tr>
+             <td>Price</td>
+            <td>
+                <asp:TextBox ID="txtPrice" runat="server"></asp:TextBox>
+
+            </td>
+        </tr>
+         <tr>
+             <td>Quantity</td>
+            <td>
+                <asp:TextBox ID="txtQuantity" runat="server"></asp:TextBox>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Button ID="btnSave" runat="server"  Text="Save" OnClick="btnSave_Click"/>
+
+            </td>
+        </tr>
+
+        
+    </table>
+</asp:Content>
+
---
+++ Admin/AddProducts.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Web.Configuration;
+using System.IO;
+
+public partial class Admin_Default : System.Web.UI.Page
+{
+    string connectionString = WebConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+    SqlConnection con;
+    SqlCommand cmd;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            fillItem();
+        }
+    }
+    public void connection()
+    {
+        con = new SqlConnection(connectionString);
+        con.Open();
+    }
+    protected void fillItem()
+    {
+        connection();
+
+        cmd = new SqlCommand("sp_ProductQuery", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@status", 1);
+        //SqlDataAdapter adp = new SqlDataAdapter(cmd);
+        //DataTable dt = new DataTable();
+        //adp.Fill(dt);
+        using (SqlDataReader dr = cmd.ExecuteReader())
+        {
+            if (dr.HasRows)
+            {
+                ddlItem.DataSource = dr;
+                ddlItem.DataValueField = "item_id";
+                ddlItem.DataTextField = "item_name";
+                ddlItem.DataBind();
+            }
+        }
+    }
+
+
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        connection();
+        string name = txtName.Text;
+        string item = ddlItem.SelectedValue;
+        string price = txtPrice.Text;
+        string qnty = txtQuantity.Text;
+        string description = txtDescription.Text;
+        string pic = Path.GetFileName(fileImage.PostedFile.FileName.ToString());
+        fileImage.SaveAs(Server.MapPath("Photos/" + pic));
+        cmd = new SqlCommand("sp_ProductQuery", con);
+        cmd.CommandType = CommandType.StoredProcedure;
+        cmd.Parameters.AddWithValue("@status", 2);
+        cmd.Parameters.AddWithValue("@product_name",name);
+        cmd.Parameters.AddWithValue("@item_id",item);
+        cmd.Parameters.AddWithValue("@product_price",price);
+        cmd.Parameters.AddWithValue("@product_description", description);
+        cmd.Parameters.AddWithValue("@product_photo",pic);
+
+
+
+        cmd.Parameters.AddWithValue("@product_quantity", qnty);
+        cmd.ExecuteNonQuery();
+
+    }
+}
